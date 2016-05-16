14fe048ad21dc4f653c5a9d25e0168f1fb37d736
cklynch2 <cklynch2@gmail.com>
2016-05-16 18:16:47 -0400

    All the holiday supply lists are in order, time to celebrate

--- ApplesAndHolidays/FISAppDelegate.m
+++ ApplesAndHolidays/FISAppDelegate.m
@@ -11,10 +11,51 @@
     return YES;
 }
 
-/**
- 
- * Implement your methods here.
- 
- */
+-(NSArray *)pickApplesFromFruits:(NSArray *)fruits
+{
+    NSPredicate *applesPredicate = [NSPredicate predicateWithFormat:@"self LIKE 'apple'"];
+    NSArray *applesOnly = [fruits filteredArrayUsingPredicate:applesPredicate];
+    return applesOnly;
+}
+
+-(NSArray *)holidaysInSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    NSDictionary *holidaysInSeasonWithSupplies = database[season];
+    NSArray *holidaysInSeason = [holidaysInSeasonWithSupplies allKeys];
+    NSLog(@"These are the holidays in the %@ season: %@", season, holidaysInSeason);
+    return holidaysInSeason;
+}
+
+-(NSArray *)suppliesInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    NSArray *suppliesInHolidayInSeason = database[season][holiday];
+    return suppliesInHolidayInSeason;
+}
+
+-(BOOL)holiday:(NSString *)holiday isInSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    BOOL isHolidayInSeason = [[self holidaysInSeason:season inDatabase:database] containsObject:holiday];
+    return isHolidayInSeason;
+}
+
+-(BOOL)supply:(NSString *)supply isInHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    BOOL isSupplyInHoliday = [[self suppliesInHoliday:holiday inSeason:season inDatabase:database] containsObject:supply];
+    return isSupplyInHoliday;
+}
+
+-(NSDictionary *)addHoliday:(NSString *)holiday toSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    NSMutableDictionary *databaseWithNewHolidayInSeason = [database mutableCopy];
+    databaseWithNewHolidayInSeason[season][holiday] = [[NSMutableArray alloc]init];
+    return databaseWithNewHolidayInSeason;
+}
+
+ -(NSDictionary *)addSupply:(NSString *)supply toHoliday:(NSString *)holiday inSeason:(NSString *)season inDatabase:(NSDictionary *)database
+{
+    NSMutableDictionary *databaseWithNewSuppliesForHoliday = [database mutableCopy];
+    databaseWithNewSuppliesForHoliday[season][holiday] = [[self suppliesInHoliday:holiday inSeason:season inDatabase:database] arrayByAddingObject:supply];
+    return databaseWithNewSuppliesForHoliday;
+}
 
 @end
